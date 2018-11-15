f6aa11ccb539cf7c58f5f51fcd92725f0d8aecf9
Xu Yiheng Stident <xuyiheng2017@berkeley.edu>
2018-11-15 15:57:53 -0600

    optimized solution

--- Two Sum.java
+++ Two Sum.java
@@ -1,14 +1,17 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int size = nums.length;
+        int[] result = new int[2];
+        HashMap<Integer, Integer> map = new HashMap();
         for (int i = 0; i < size; i++) {
-            for (int j = i + 1; j < size; j++) {
-                if (nums[j] == target - nums[i]) {
-                    int[] result = new int[2];
-                    result[0] = i;
-                    result[1] = j;
-                    return result;
-                }
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < size; i++) {
+            Integer j = map.get(target - nums[i]);
+            if (j != null && j != i) {
+                result[0] = i;
+                result[1] = j;
+                return result;
             }
         }
         return null;
